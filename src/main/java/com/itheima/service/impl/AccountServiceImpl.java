f9413fefd1e384d8d96808851eb9651493cfead3
LeeMJ <Leeminjie1995@126.com>
2020-10-10 21:20:06 +0800

    firstTest

--- src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -9,7 +9,7 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void saveAccout() {
         System.out.println("执行了保存");
-        int i = 1/0;
+//        int i = 1/0;
         System.out.println("执行了保存2");
     }
 
